27719aee97aee1edb38aaaab950edaf87817cd5d
miaobobo <2586277936@qq.com>
2026-07-04 13:53:22 +0800

    Edit model trainer arguments.

--- Sources/model/training.py
+++ Sources/model/training.py
@@ -3,7 +3,7 @@ os.environ["HF_ENDPOINT"]="https://hf-mirror.com"
 
 import json
 from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, DataCollatorForLanguageModeling
-from peft import LoraConfig, get_peft_model, TaskType
+from peft import LoraConfig, TaskType
 from datasets import Dataset
 
 if __name__ == "__main__":
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(MODELNAME, trust_remote_code=True)
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
-    model = AutoModelForCausalLM.from_pretrained(MODELNAME, trust_remote_code=True, dtype="auto", device_map="cpu")
+    model = AutoModelForCausalLM.from_pretrained(MODELNAME, trust_remote_code=True, dtype="auto", device_map="auto")
 
     def loadJson(filePath):
         with open(filePath, 'r', encoding='utf-8') as f:
@@ -23,11 +23,11 @@ if __name__ == "__main__":
     for data in dataOfQues:
         messages = [
             {"role": "system", "content": "你是一个出卷老师，请按照科目、单元、年级、题型出题。题型的类型有choose, fillBlank, answer，分别对应选择/判断、填空、实验探究/解答/综合"}, 
-            {"role": "user", "content": f"学科：{data['subject']}，年级：{data['grade'][1]}年级{"上册" if data['grade'][0] == 'A' else "下册"}，单元：{data['unit']}，题型：{data['type']}"}, 
+            {"role": "user", "content": f"学科：{data['subject']}，年级：{data['grade'][1]}年级{'上册' if data['grade'][0] == 'A' else '下册'}，单元：{data['unit']}，题型：{data['type']}"}, 
             {"role": "assistant", "content": data['output']}
         ]
         inputID = tokenizer.apply_chat_template(messages, tokenize=True, generation_promat=False)
-        formattedDataOfQues.append(inputID)
+        formattedDataOfQues.append({"input_ids": inputID})
     datasetOfQues = Dataset.from_list(formattedDataOfQues)
     formattedDataOfGrade = []
     for data in dataOfGrade:
@@ -38,7 +38,7 @@ if __name__ == "__main__":
             {"role": "assistant", "content": output}
         ]
         inputID = tokenizer.apply_chat_template(messages, tokenize=True, generation_promat=False)
-        formattedDataOfGrade.append(inputID)
+        formattedDataOfGrade.append({"input_ids": inputID})
     datasetOfGrade = Dataset.from_list(formattedDataOfGrade)
     peftConfig = LoraConfig(
         task_type=TaskType.CAUSAL_LM, 
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     model.save_pretrained("./Sources/model/outputQues")
 
     model.set_adapter("grade")
-    trainArgsForQues = TrainingArguments(
+    trainArgsForGrade = TrainingArguments(
         output_dir="./Sources/model/outputGrade", 
         num_train_epochs=3, 
         per_device_train_batch_size=1, 
@@ -89,11 +89,11 @@ if __name__ == "__main__":
         dataloader_num_workers=0, 
         disable_tqdm=False
     )
-    trainerForQues = Trainer(
+    trainerForGrade = Trainer(
         model=model, 
-        args=trainArgsForQues, 
-        train_dataset=datasetOfQues, 
+        args=trainArgsForGrade, 
+        train_dataset=datasetOfGrade, 
         data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False)
     )
-    trainerForQues.train()
+    trainerForGrade.train()
     model.save_pretrained("./Sources/model/outputGrade")
