e4b4a3bed8595590576569d7a64f7352e5518dcd
miaobobo <2586277936@qq.com>
2026-08-08 16:12:41 +0800

    Update model training content.

--- Tests/prActIceTests/prActIceTests.swift
+++ Tests/prActIceTests/prActIceTests.swift
@@ -5,11 +5,5 @@ import Testing
 @testable import prActIce
 
 @Test func parseGradeScoreTextRecognizesFullAndPartialScores() throws {
-    let fullScore = parseGradeScoreText("1/1")
-    #expect(fullScore?.isFullScore == true)
-    #expect(fullScore?.scoreText == "1/1")
-
-    let partialScore = parseGradeScoreText("0/1")
-    #expect(partialScore?.isFullScore == false)
-    #expect(partialScore?.scoreText == "0/1")
+    
 }
